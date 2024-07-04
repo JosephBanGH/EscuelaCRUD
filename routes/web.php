1e2a35e7beaf76c43c7790c04aae2ef08e95f6f2
joseplocal <joseplocal>
2024-07-04 00:14:13 -0500

    solucione login ruta

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ use App\Http\Controllers\UserController;
 Route::get('/', [UserController::class,'showLogin']);
 Route::post('/identificacion', [UserController::class,'verificalogin'])->name('identificacion');
 Route::post('/salir', [UserController::class,'salir'])->name('logout');
-Route::get('/login', function () {
+Route::get('/login/login', function () {
     return view('login');
 });
