7462f52b7ec8b6d0231324a5e6acdd726363f9c6
AndyDemVikal <vermouthsama12@gmail.com>
2024-07-03 23:52:13 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
+
+/* Route::get('/', function () {
     return view('prueba');
-});
+}); */
 
+Route::get('/', [UserController::class,'showLogin']);
+Route::post('/identificacion', [UserController::class,'verificalogin'])->name('identificacion');
+Route::post('/salir', [UserController::class,'salir'])->name('logout');
